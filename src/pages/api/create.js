f1cd17918b7e6130d44fa6b3792e482b39651b67
ATOM03 <priyanshujhaginia@outlook.com>
2024-03-24 02:17:12 +0530

    Setup the Backend

---
+++ src/pages/api/create.js
@@ -0,0 +1,32 @@
+import { createClient } from "@supabase/supabase-js";
+import { nanoid } from "nanoid";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { originalUrl } = req.body;
+    const id = nanoid(8);
+
+    try {
+      const { data, error } = await supabase
+        .from("urls")
+        .insert([{ id, original_url: originalUrl }])
+        .single();
+
+      console.log(data, error);
+
+      if (error) {
+        res.status(500).json({ error: "Failed to create short URL" });
+      } else {
+        const shortUrl = `${req.headers.host}/${data.id}`;
+        res.status(200).json({ shortUrl });
+      }
+    } catch (error) {
+      res.status(405).json({ error: error });
+    }
+  }
+}
